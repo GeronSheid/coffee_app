90405b59ac1963d5e4b40e704edc1d252b996601
Dmitry Shneider <dmitryshneider@gmail.com>
2024-09-18 23:58:02 +0600

    Настройка импорта axios

--- api/axios.ts
+++ api/axios.ts
@@ -1,11 +1,12 @@
-import axios from "axios";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const axios = require('axios');
 
 const apiClient = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+    baseURL: process.env.NEXT_PUBLIC_BASE_UR || 'http://localhost:3000/api',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
     },
 })
 
-export default apiClient;
+module.exports = apiClient;
