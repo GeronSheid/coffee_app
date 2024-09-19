97db071fdf411f6de6732d25ca35b93bf708370c
Dmitry Shneider <dmitryshneider@gmail.com>
2024-09-19 19:31:58 +0600

    И еще одно исправление

--- api/coffees.ts
+++ api/coffees.ts
@@ -1,5 +1,5 @@
 import { ICoffeeCard } from "@/components/CoffeeCard";
-import apiClient from "./axios";
+import apiClient from "./axios.ts";
 
 export const getCoffees = async (page: number = 1, limit: number = 10) => {
     try {
