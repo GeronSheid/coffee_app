5e6644558b3aa3fa85761c63327c203559bc3312
Dmitry Shneider <dmitryshneider@gmail.com>
2024-09-20 12:19:58 +0600

    Настроены пути

--- components/CoffeCards.tsx
+++ components/CoffeCards.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { CoffeeCard, ICoffeeCard } from './CoffeeCard'
-import { getCoffees, getCoffeesNext } from '@/api/coffees'
+import { getCoffees, getCoffeesNext } from '@/api/coffeesAPI'
 import { Skeleton } from './ui/skeleton'
 import { DynamicPagination } from './DynamicPagination'
 import { TopBar } from './TopBar'
