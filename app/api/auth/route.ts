06d379325d61834e805470896b1834440023c48e
Dmitry Shneider <dmitryshneider@gmail.com>
2024-10-01 19:18:11 +0600

    настройка для CORS

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -3,6 +3,19 @@ import bcrypt from 'bcryptjs';
 import { generateAccessToken, generateRefreshToken } from "@/lib/jwt";
 import { NextRequest, NextResponse } from "next/server";
 import { setAuthCookie } from "@/app/actions/auth";
+import { headers } from "next/headers";
+
+export async function OPTIONS() {
+    // Устанавливаем CORS заголовки для preflight запроса
+    return new Response(null, {
+        status: 200,
+        headers: {
+        'Access-Control-Allow-Origin': '*', // Разрешить все источники
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        },
+    });
+    }
 
 export async function POST(request: NextRequest) {
     const {email, password} = await request.json()
@@ -16,26 +29,7 @@ export async function POST(request: NextRequest) {
     }
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
-    const response = NextResponse.json({message: 'Логин успешен', data: {accesToken: accessToken}})
+    const response = NextResponse.json({message: 'Логин успешен', data: {accesToken: accessToken}, headers: {'Access-Control-Allow-Origin': '*', // Разрешить все источники
+      'Content-Type': 'application/json',}})
     setAuthCookie(request, response, refreshToken);
 }
-
-
-// export async function signInHandler(data: {email: string, password: string}) {
-//     const {email, password} = data;
-//     const user = await prisma.user.findUnique({ where: {email: email}})
-//     if(!user) {
-//         throw new Error('Пользователь с этим email не обнаружен')
-//     }
-//     const isMatch = await bcrypt.compare(password, user.password)
-//     if(!isMatch) {
-//         throw new Error('Неправильный пароль')
-//     }
-
-//     const accessToken = generateAccessToken(user);
-//     const refreshToken = generateRefreshToken(user);
-
-
-//     const res = NextResponse.json({message: 'Успешно', })
-//     setAuthCookie()
-// }
