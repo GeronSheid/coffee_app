6899e102546d29dc9a93e8fe6f721a7e20d2227c
Dmitry Shneider <dmitryshneider@gmail.com>
2024-10-01 19:22:39 +0600

    Откат CORS

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -3,19 +3,7 @@ import bcrypt from 'bcryptjs';
 import { generateAccessToken, generateRefreshToken } from "@/lib/jwt";
 import { NextRequest, NextResponse } from "next/server";
 import { setAuthCookie } from "@/app/actions/auth";
-import { headers } from "next/headers";
 
-export async function OPTIONS() {
-    // Устанавливаем CORS заголовки для preflight запроса
-    return new Response(null, {
-        status: 200,
-        headers: {
-        'Access-Control-Allow-Origin': '*', // Разрешить все источники
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        },
-    });
-    }
 
 export async function POST(request: NextRequest) {
     const {email, password} = await request.json()
@@ -29,7 +17,6 @@ export async function POST(request: NextRequest) {
     }
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
-    const response = NextResponse.json({message: 'Логин успешен', data: {accesToken: accessToken}, headers: {'Access-Control-Allow-Origin': '*', // Разрешить все источники
-      'Content-Type': 'application/json',}})
+    const response = NextResponse.json({message: 'Логин успешен', data: {accesToken: accessToken}})
     setAuthCookie(request, response, refreshToken);
 }
