344d6c52a5dd243bce865b28f145a4441342eb44
Dmitry Shneider <dmitryshneider@gmail.com>
2024-10-01 19:32:53 +0600

    Обработка ошибок при авторизации

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -6,17 +6,22 @@ import { setAuthCookie } from "@/app/actions/auth";
 
 
 export async function POST(request: NextRequest) {
-    const {email, password} = await request.json()
-    const user = await prisma.user.findUnique({where: {email: email}})
-    if(!user) {
-        throw new Error('Пользователь не найден')
+    try {
+        const { email, password } = await request.json()
+        const user = await prisma.user.findUnique({ where: { email: email } })
+        if (!user) {
+            return NextResponse.json({ message: 'Пользовательн не найден' }, { status: 404 })
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch) {
+            return NextResponse.json({ message: 'Неверный пароль' }, { status: 401 })
+        }
+        const accessToken = generateAccessToken(user);
+        const refreshToken = generateRefreshToken(user);
+        const response = NextResponse.json({ message: 'Логин успешен', data: { accesToken: accessToken } })
+        setAuthCookie(request, response, refreshToken);
+    } catch (error) {
+        console.error('Ошибка при аутентификации:', error);
+        return NextResponse.json({ error: 'Произошла ошибка при аутентификации' }, { status: 500 });
     }
-    const isMatch = await bcrypt.compare(password, user.password)
-    if(!isMatch) {
-        throw new Error('Неверный пароль')
-    }
-    const accessToken = generateAccessToken(user);
-    const refreshToken = generateRefreshToken(user);
-    const response = NextResponse.json({message: 'Логин успешен', data: {accesToken: accessToken}})
-    setAuthCookie(request, response, refreshToken);
 }
